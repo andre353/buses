b6d63df1b879c056038a6b886c85acc6ace67556
andre353 <eva595eva@gmail.com>
2024-08-22 15:13:37 +0300

    read json file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import {readFile} from 'node:fs/promises';
 const port = 3000;
 
 const app = express();
@@ -7,6 +8,15 @@ app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
 
-app.get("/hello", (req, res) => {
+app.get("/next-departure", async (req, res) => {
+
     res.send('Hello World!');
 })
+
+const loadBuses = async () => {
+    const data = await readFile('./buses.json', 'utf8');
+    // return JSON.parse(data);
+    console.log("data: ", data);
+    
+}
+loadBuses();
